e2504e262bda2311e5dc185d71b9efebb9ad2aa5
John Fallows <john.fallows@kaazing.com>
2019-02-23 00:04:07 -0800

    Initial implementation

---
+++ src/main/java/org/reaktivity/nukleus/fan/internal/FanNukleus.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2016-2018 The Reaktivity Project
+ *
+ * The Reaktivity Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package org.reaktivity.nukleus.fan.internal;
+
+import org.reaktivity.nukleus.Nukleus;
+
+final class FanNukleus implements Nukleus
+{
+    static final String NAME = "fan";
+
+    private final FanConfiguration config;
+
+    FanNukleus(
+        FanConfiguration config)
+    {
+        this.config = config;
+    }
+
+    @Override
+    public String name()
+    {
+        return FanNukleus.NAME;
+    }
+
+    @Override
+    public FanConfiguration config()
+    {
+        return config;
+    }
+
+    @Override
+    public FanElektron supplyElektron()
+    {
+        return new FanElektron(config);
+    }
+}
